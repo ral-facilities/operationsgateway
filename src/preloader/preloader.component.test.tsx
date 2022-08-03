d753dfbb9064134b4fccc2faa2bc9ca48297711c
Sam Glendenning <sam.glendenning@stfc.ac.uk>
2022-08-03 15:49:13 +0100

    DSEGOG-4 refactor preloader tests
    Now use RTL

--- src/preloader/preloader.component.test.tsx
+++ src/preloader/preloader.component.test.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Preloader from './preloader.component';
-import { shallow } from 'enzyme';
-
-// TODO refactor this to use RTL
+import { render } from '@testing-library/react';
 
 describe('Preloader component', () => {
+  const createView = (loading: boolean) => {
+    return render(<Preloader loading={loading} children={<div>Test</div>} />);
+  };
+
   it('renders when the site is loading', () => {
-    const wrapper = shallow(<Preloader loading={true} />);
-    expect(wrapper).toMatchSnapshot();
+    const view = createView(true);
+    expect(view.asFragment()).toMatchSnapshot();
   });
 
   it('does not render when the site is not loading', () => {
-    const wrapper = shallow(<Preloader loading={false} />);
-    expect(wrapper).toMatchSnapshot();
+    const view = createView(false);
+    expect(view.asFragment()).toMatchSnapshot();
   });
 });
