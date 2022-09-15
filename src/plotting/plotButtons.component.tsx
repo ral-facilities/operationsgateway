7d2d485a0c5d44781600b9dcac2e6da0af8d3734
Sam Glendenning <sam.glendenning@stfc.ac.uk>
2022-09-15 16:43:25 +0100

    DSEGOG-52 bumping coverage for plot buttons

--- src/plotting/plotButtons.component.tsx
+++ src/plotting/plotButtons.component.tsx
@@ -37,6 +37,75 @@ type DataRow = {
   [column: string]: number;
 };
 
+export const constructDataRows = (
+  XAxis: string,
+  plots: PlotDataset[]
+): (string | number)[][] => {
+  // First row of file, i.e. the column names
+  const headerRow = [XAxis].concat(plots.map((plot) => plot.name));
+
+  const dataRows: DataRow[] = [];
+
+  // For each plotted dataset, extract data points and add to array of data rows
+  plots.forEach((plot) => {
+    const plotDataset = plot.data;
+    for (let i = 0; i < plotDataset.length; i++) {
+      const currentPoint: { [x: string]: number } = plotDataset[i];
+
+      // Extract the X and Y values for this point in the dataset
+      const currentPointXVal = currentPoint[XAxis];
+      const currentPointYVal = currentPoint[plot.name];
+
+      // See if we already have a data row matching this X value
+      const matchingDataRow = dataRows.find(
+        (dataRow) => dataRow[XAxis] === currentPointXVal
+      );
+      let newDataRow: DataRow;
+      if (matchingDataRow) {
+        // Found matching data row, add a "column" to it for this piece of data
+        newDataRow = {
+          ...matchingDataRow,
+
+          [plot.name]: currentPointYVal,
+        };
+        dataRows[dataRows.indexOf(matchingDataRow)] = newDataRow;
+      } else {
+        // Make a new data row for this X value
+        newDataRow = {
+          [XAxis]: currentPointXVal,
+
+          [plot.name]: currentPointYVal,
+        };
+        dataRows.push(newDataRow);
+      }
+    }
+  });
+
+  // Sort by X value ascending
+  const sortedDataRows = dataRows.sort((row1, row2) => {
+    const row1XVal = row1[XAxis];
+    const row2XVal = row2[XAxis];
+
+    if (row1XVal > row2XVal) return 1;
+    return -1;
+  });
+
+  // Transform the DataRow array into a 2D array of data, corresponding to rows in CSV
+  // This is usually numbered data but is string in the case of formatted timestamps
+  const finalCsvRows: (string | number)[][] = sortedDataRows.map((dataRow) => {
+    return headerRow.map((header) => {
+      if (Object.keys(dataRow).includes(header)) {
+        return header === 'timestamp'
+          ? formatTooltipLabel(dataRow[header], 'time')
+          : dataRow[header];
+      }
+      return '';
+    });
+  });
+
+  return [headerRow, ...finalCsvRows];
+};
+
 /**
  *  Exports the graph data as a CSV
  *  @param data The data to export
@@ -44,71 +113,7 @@ type DataRow = {
  */
 function exportData(title: string, XAxis: string, plots?: PlotDataset[]): void {
   if (plots && plots.length > 0) {
-    // First row of file, i.e. the column names
-    const headerRow = [XAxis].concat(plots.map((plot) => plot.name));
-
-    // Array of rows following the first row, i.e. the actual data
-    const dataRows: DataRow[] = [];
-
-    // For each plotted dataset, extract data points and add to array of data rows
-    plots.forEach((plot) => {
-      const plotDataset = plot.data;
-      for (let i = 0; i < plotDataset.length; i++) {
-        const currentPoint: { [x: string]: number } = plotDataset[i];
-
-        // Extract the X and Y values for this point in the dataset
-        const currentPointXVal = currentPoint[XAxis];
-        const currentPointYVal = currentPoint[plot.name];
-
-        // See if we already have a data row matching this X value
-        const matchingDataRow = dataRows.find(
-          (dataRow) => dataRow[XAxis] === currentPointXVal
-        );
-        let newDataRow: DataRow;
-        if (matchingDataRow) {
-          // Found matching data row, add a "column" to it for this piece of data
-          newDataRow = {
-            ...matchingDataRow,
-
-            [plot.name]: currentPointYVal,
-          };
-          dataRows[dataRows.indexOf(matchingDataRow)] = newDataRow;
-        } else {
-          // Make a new data row for this X value
-          newDataRow = {
-            [XAxis]: currentPointXVal,
-
-            [plot.name]: currentPointYVal,
-          };
-          dataRows.push(newDataRow);
-        }
-      }
-    });
-
-    // Sort by X value ascending
-    const sortedDataRows = dataRows.sort((row1, row2) => {
-      const row1XVal = row1[XAxis];
-      const row2XVal = row2[XAxis];
-
-      if (row1XVal > row2XVal) return 1;
-      if (row1XVal < row2XVal) return -1;
-      return 0;
-    });
-
-    // Transform the DataRow array into a 2D array of data, corresponding to rows in CSV
-    // This is usually numbered data but is string in the case of formatted timestamps
-    const finalCsvRows = sortedDataRows.map((dataRow) => {
-      return headerRow.map((header) => {
-        if (Object.keys(dataRow).includes(header)) {
-          return header === 'timestamp'
-            ? formatTooltipLabel(dataRow[header], 'time')
-            : dataRow[header];
-        }
-        return '';
-      });
-    });
-
-    const csvArray = [headerRow, ...finalCsvRows];
+    const csvArray = constructDataRows(XAxis, plots);
 
     const csvContent =
       'data:text/csv;charset=utf-8,' +
