376cba9afdf1f28131c3cd8f9e0aeef5285fe0d4
Louise Davies <louise.davies@stfc.ac.uk>
2022-10-19 10:14:30 +0100

    DSEGOG-104 add some buffer to drag and drop command - this ensures it works when dragging columns, as previously it broke

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,39 +31,39 @@ Cypress.Commands.add('dragAndDrop', (subject, target) => {
     consoleProps: () => {
       return {
         subject: subject,
-        target: target
+        target: target,
       };
-    }
+    },
   });
   const BUTTON_INDEX = 0;
   const SLOPPY_CLICK_THRESHOLD = 10;
   cy.get(target)
     .first()
-    .then($target => {
+    .then(($target) => {
       let coordsDrop = $target[0].getBoundingClientRect();
       cy.get(subject)
         .first()
-        .then(subject => {
+        .then((subject) => {
           const coordsDrag = subject[0].getBoundingClientRect();
           cy.wrap(subject)
             .trigger('mousedown', {
               button: BUTTON_INDEX,
               clientX: coordsDrag.x,
               clientY: coordsDrag.y,
-              force: true
+              force: true,
             })
             .trigger('mousemove', {
               button: BUTTON_INDEX,
               clientX: coordsDrag.x + SLOPPY_CLICK_THRESHOLD,
               clientY: coordsDrag.y,
-              force: true
+              force: true,
             });
           cy.get('body')
             .trigger('mousemove', {
               button: BUTTON_INDEX,
-              clientX: coordsDrop.x,
-              clientY: coordsDrop.y,
-              force: true
+              clientX: coordsDrop.x + SLOPPY_CLICK_THRESHOLD,
+              clientY: coordsDrop.y + SLOPPY_CLICK_THRESHOLD,
+              force: true,
             })
             .trigger('mouseup');
         });
