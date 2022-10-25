19fbd764a1ca526d887c958298bcb5adcc5454c7
Sam Glendenning <sam.glendenning@stfc.ac.uk>
2022-10-25 16:41:31 +0100

    DSEGOG-174 improving deep copy method
    JSON.parse with JSON.stringify is more reliable in this case

--- src/plotting/util.tsx
+++ src/plotting/util.tsx
@@ -1,13 +1,7 @@
 import { SelectedPlotChannel } from '../app.types';
 
-// TODO test this
 export const deepCopySelectedPlotChannels = (
   original: SelectedPlotChannel[]
 ): SelectedPlotChannel[] => {
-  return original.map((currentChannel: SelectedPlotChannel) => ({
-    name: currentChannel.name,
-    options: {
-      ...currentChannel.options,
-    },
-  }));
+  return JSON.parse(JSON.stringify(original));
 };
