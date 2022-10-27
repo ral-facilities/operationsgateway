743ae5529cdde3931a8ab46c0c7033e13bdb3676
Sam Glendenning <sam.glendenning@stfc.ac.uk>
2022-10-27 15:57:40 +0100

    DSEGOG-106 updating tableButtons tests

--- src/views/tableButtons.component.test.tsx
+++ src/views/tableButtons.component.test.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import TableButtons from './tableButtons.component';
-import { act, render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import axios from 'axios';
+import userEvent from '@testing-library/user-event';
 
 describe('Table buttons', () => {
   const openFilters = jest.fn();
+  let user;
 
   const createView = () => {
     return render(<TableButtons openFilters={openFilters} />);
   };
 
   beforeEach(() => {
+    user = userEvent.setup();
     (axios.get as jest.Mock).mockResolvedValue({ data: [] });
   });
 
   it('renders the buttons', async () => {
-    let view;
-    await act(async () => {
-      view = createView();
-    });
+    const view = createView();
     expect(view.asFragment()).toMatchSnapshot();
+  });
+
+  it('calls openFilters when the filters button is clicked', async () => {
+    createView();
+
+    await user.click(screen.getByRole('button', { name: 'Filters' }));
+    expect(openFilters).toHaveBeenCalled();
   });
 });
