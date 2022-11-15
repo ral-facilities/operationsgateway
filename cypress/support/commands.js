aed4975bcc3055851d64325525bf017b69b575bf
Sam Glendenning <sam.glendenning@stfc.ac.uk>
2022-11-15 14:44:08 +0000

    DSEGOG-95 increasing SLOPPY_CLICK_THRESHOLD
    It could be because I've slightly amended the  dataHeader widths but the drag and drop functionality in e2e tests was failing. I've increased the threshold for clicking and dragging to try and fix this

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -36,7 +36,7 @@ Cypress.Commands.add('dragAndDrop', (subject, target) => {
     },
   });
   const BUTTON_INDEX = 0;
-  const SLOPPY_CLICK_THRESHOLD = 10;
+  const SLOPPY_CLICK_THRESHOLD = 30;
   cy.get(target)
     .first()
     .then(($target) => {
