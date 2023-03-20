0aef939094106283d74f239ab71ccf7f948a3525
Louise Davies <louise.davies@stfc.ac.uk>
2023-03-20 15:50:53 +0000

    DSEGOG-125 add e2e tests for false colour

--- e2e/images.spec.ts
+++ e2e/images.spec.ts
@@ -66,7 +66,7 @@ test('user can zoom and pan the image', async ({ page }) => {
     await imageDiv.screenshot({
       type: 'png',
     })
-  ).toMatchSnapshot();
+  ).toMatchSnapshot({ maxDiffPixels: 150 });
 
   // test that multiple zoom levels work
   await imageDiv.dragTo(imageDiv, {
@@ -84,7 +84,7 @@ test('user can zoom and pan the image', async ({ page }) => {
     await imageDiv.screenshot({
       type: 'png',
     })
-  ).toMatchSnapshot();
+  ).toMatchSnapshot({ maxDiffPixels: 150 });
 
   await popup.locator('text=Reset View').click();
 
@@ -92,5 +92,101 @@ test('user can zoom and pan the image', async ({ page }) => {
     await imageDiv.screenshot({
       type: 'png',
     })
+  ).toMatchSnapshot({ maxDiffPixels: 150 });
+});
+
+test('user can change the false colour parameters of an image', async ({
+  page,
+}) => {
+  // open up popup
+  const [popup] = await Promise.all([
+    page.waitForEvent('popup'),
+    page.getByAltText('Channel_BCDEF image', { exact: false }).first().click(),
+  ]);
+
+  const title = await popup.title();
+  const imgAltText = title.split(' - ')[1];
+
+  const image = await popup.getByAltText(imgAltText);
+  const colourbar = await popup.getByAltText('Colour bar');
+
+  await popup.getByLabel('Colour Map').click();
+
+  await popup.getByRole('option', { name: 'colourmap_2' }).click();
+
+  const llSlider = await popup.getByRole('slider', {
+    name: 'Lower Level (LL)',
+  });
+  const llSliderRoot = await popup.locator('.MuiSlider-root', {
+    has: llSlider,
+  });
+
+  const sliderDims = await llSliderRoot.boundingBox();
+
+  await llSliderRoot.dragTo(llSliderRoot, {
+    targetPosition: {
+      // moving the slider to the target value in %
+      x: (sliderDims?.width ?? 0) * 0.4,
+      y: sliderDims?.height ? sliderDims.height / 2 : 0,
+    },
+  });
+
+  expect(await llSlider.getAttribute('value')).toBe(`${0.4 * 255}`);
+
+  const ulSlider = await popup.getByRole('slider', {
+    name: 'Upper Level (UL)',
+  });
+  const ulSliderRoot = await popup.locator('.MuiSlider-root', {
+    has: ulSlider,
+  });
+
+  await ulSliderRoot.click({
+    position: {
+      x: (sliderDims?.width ?? 0) * 0.6,
+      y: sliderDims?.height ? sliderDims.height / 2 : 0,
+    },
+  });
+
+  expect(await ulSlider.getAttribute('value')).toBe(`${0.6 * 255}`);
+  // blur to avoid focus tooltip appearing in snapshot
+  await ulSlider.blur();
+
+  // wait for new image to have loaded
+  await image.click();
+
+  expect(
+    await image.screenshot({
+      type: 'png',
+    })
+  ).toMatchSnapshot({ maxDiffPixels: 150 });
+
+  expect(
+    await colourbar.screenshot({
+      type: 'png',
+    })
   ).toMatchSnapshot();
+});
+
+test('user can disable false colour', async ({ page }) => {
+  // open up popup
+  const [popup] = await Promise.all([
+    page.waitForEvent('popup'),
+    page.getByAltText('Channel_BCDEF image', { exact: false }).first().click(),
+  ]);
+
+  const title = await popup.title();
+  const imgAltText = title.split(' - ')[1];
+
+  const image = await popup.getByAltText(imgAltText);
+
+  await popup.getByRole('checkbox', { name: 'False colour' }).click();
+
+  // wait for new image to have loaded
+  await image.click();
+
+  expect(
+    await image.screenshot({
+      type: 'png',
+    })
+  ).toMatchSnapshot({ maxDiffPixels: 150 });
 });
