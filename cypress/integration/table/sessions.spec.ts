2d3a7d7859574a07e7245c938aa74d329f2d3edd
Joshua Kitenge <83226114+joshuadkitenge@users.noreply.github.com>
2023-06-15 13:19:45 +0100

    DSEGOG-129 add e2e test

---
+++ cypress/integration/table/sessions.spec.ts
@@ -0,0 +1,50 @@
+function getParamsFromUrl(url: string) {
+  const paramsString = url.split('?')[1];
+  const paramMap = new Map();
+  paramsString.split('&').forEach(function (part) {
+    const keyValPair = part.split('=');
+    const key = keyValPair[0];
+    const val = decodeURIComponent(keyValPair[1]);
+    paramMap.set(key, val);
+  });
+  return paramMap;
+}
+
+describe('Sessions', () => {
+  afterEach(() => {
+    cy.clearMocks();
+  });
+  it('sends a posts a request when a user session is created', () => {
+    cy.visit('/');
+
+    cy.findByTestId('AddCircleIcon').click();
+    cy.findByLabelText('Name*').type('Session');
+    cy.findByLabelText('Summary').type('Summary');
+
+    cy.startSnoopingBrowserMockedRequest();
+
+    cy.findByRole('button', { name: 'Save' }).click();
+
+    cy.findBrowserMockedRequests({ method: 'POST', url: '/sessions' }).should(
+      (patchRequests) => {
+        expect(patchRequests.length).equal(1);
+        const request = patchRequests[0];
+        expect(request.body.toString()).equal(
+          '{"table":{"columnStates":{},"selectedColumnIds":["timestamp"],"page":0,"resultsPerPage":25,"sort":{}},"search":{"searchParams":{"dateRange":{},"shotnumRange":{},"maxShots":50,"experimentID":null}},"plots":{},"filter":{"appliedFilters":[[]]},"windows":{}}'
+        );
+
+        expect(request.url.toString()).to.contain('name=');
+        expect(request.url.toString()).to.contain('summary=');
+        expect(request.url.toString()).to.contain('auto_saved=');
+
+        const paramMap: Map<string, string> = getParamsFromUrl(
+          request.url.toString()
+        );
+
+        expect(paramMap.get('name')).equal('Session');
+        expect(paramMap.get('summary')).equal('Summary');
+        expect(paramMap.get('auto_saved')).equal('false');
+      }
+    );
+  });
+});
