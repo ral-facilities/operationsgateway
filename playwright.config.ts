23a874c2d4a14f0f36e1b9e6271183f018e1fb41
Kenneth <kenneth.ng.nym@outlook.com>
2023-08-10 12:57:53 +0100

    Revert "Add npm script for updating playwright snapshots"

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,20 +30,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // use 0.0.0.0 loopback address when running the tests in a local docker container
-  // so that the report URL is accessible via host localhost:9323.
-  reporter: process.env.LOCAL_DOCKER
-    ? [
-        [
-          'html',
-          {
-            open: 'never',
-            host: '0.0.0.0',
-            port: 9323,
-          },
-        ],
-      ]
-    : 'html',
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
