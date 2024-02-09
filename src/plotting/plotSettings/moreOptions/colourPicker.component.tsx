7fd261cea7d31e646abb98293d25d395a1483fc9
Kacper Roemer <kacperroemer@gmail.com>
2024-02-09 14:29:18 +0000

    fix unit tests

--- src/plotting/plotSettings/moreOptions/colourPicker.component.tsx
+++ src/plotting/plotSettings/moreOptions/colourPicker.component.tsx
@@ -37,7 +37,7 @@ const ColourPicker = (props: ColourPickerProps) => {
         }}
         component="button"
         onClick={() => toggle(!isOpen)}
-        aria-label={`Pick ${channelName} colour`}
+        aria-label={`Pick ${channelName}${marker ? ' marker' : ''} colour`}
         aria-haspopup="dialog"
       />
 
