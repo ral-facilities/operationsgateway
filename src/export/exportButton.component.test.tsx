6de1c6a1dc82a2b0709285286542da0eedfeebcb
Kacper Roemer <kacperroemer@gmail.com>
2024-03-07 08:20:33 +0000

    better coverage for export button

--- src/export/exportButton.component.test.tsx
+++ src/export/exportButton.component.test.tsx
@@ -1,18 +1,25 @@
-import { screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
 import { renderComponentWithProviders } from '../setupTests';
 import ExportButton from './exportButton.component';
 import React from 'react';
 
-it('renders ExportButton component', () => {
-  renderComponentWithProviders(<ExportButton />);
-  const exportButton = screen.getByText('Export');
-  expect(exportButton).toBeInTheDocument();
-});
+describe('ExportButton', () => {
+  it('opens and closes the dialog on button clicks', async () => {
+    renderComponentWithProviders(<ExportButton />);
 
-it('opens export dialogue on button click', () => {
-  renderComponentWithProviders(<ExportButton />);
-  const exportButton = screen.getByText('Export');
-  fireEvent.click(exportButton);
-  const exportDialogue = screen.getByRole('dialog');
-  expect(exportDialogue).toBeInTheDocument();
+    fireEvent.click(screen.getByText('Export'));
+    expect(screen.getByRole('dialog')).toBeInTheDocument();
+    fireEvent.click(screen.getByRole('button', { name: 'Cancel' }));
+    await waitFor(() => {
+      expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+    });
+  });
+
+  it('opens export dialogue on button click', () => {
+    renderComponentWithProviders(<ExportButton />);
+    const exportButton = screen.getByText('Export');
+    fireEvent.click(exportButton);
+    const exportDialogue = screen.getByRole('dialog');
+    expect(exportDialogue).toBeInTheDocument();
+  });
 });
