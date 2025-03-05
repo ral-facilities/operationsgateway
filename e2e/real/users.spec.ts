d813e3424dbc9178762d945f0b949edc132383fc
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2025-03-05 13:15:20 +0000

    real api tests

---
+++ e2e/real/users.spec.ts
@@ -0,0 +1,154 @@
+import { test as base, BrowserContext, expect, Page } from '@playwright/test';
+
+const adminAuthFile = 'e2e/real/.auth/admin.json';
+
+// Extend base test to authenticate only for this suite
+const test = base.extend<{ adminPage: Page }>({
+  adminPage: async ({ browser, request }, provide) => {
+    const context: BrowserContext = await browser.newContext();
+    const page: Page = await context.newPage();
+
+    await page.goto('/');
+    const response = await request.get('/operationsgateway-settings.json');
+    const { apiUrl } = await response.json();
+
+    // Perform login as admin
+    const loginResponse = await request.post(`${apiUrl}/login`, {
+      data: {
+        username: 'backend',
+        password: 'back', // Replace with actual admin credentials
+      },
+    });
+
+    const token = await loginResponse.json();
+
+    await page.evaluate((token) => {
+      window.localStorage.setItem('scigateway:token', token);
+    }, token);
+
+    // Save the admin storage state
+    await context.storageState({ path: adminAuthFile });
+    await context.close();
+
+    // Use the authenticated session
+    const adminContext: BrowserContext = await browser.newContext({
+      storageState: adminAuthFile,
+    });
+    const adminPage: Page = await adminContext.newPage();
+
+    await provide(adminPage);
+  },
+});
+
+test('should be able to create a user, read it, edit it and delete it', async ({
+  adminPage,
+}) => {
+  test.slow(); // Since it does full CRUD
+  await adminPage.goto('/admin/users');
+
+  // Add a local user
+
+  await adminPage.getByRole('button', { name: 'Add User' }).click();
+
+  await adminPage
+    .getByLabel('Username *', { exact: true })
+    .fill('frontend_e2e_local');
+
+  await adminPage
+    .getByLabel('Password *', { exact: true })
+    .fill('frontend_e2e_local_password');
+
+  await adminPage.getByLabel('Authorised Routes', { exact: true }).click();
+
+  await adminPage.getByRole('option', { name: '/users GET' }).click();
+
+  await adminPage.getByRole('button', { name: 'Submit' }).click();
+
+  // Add a fed Id user
+
+  await adminPage.getByRole('button', { name: 'Add User' }).click();
+
+  await adminPage.getByLabel('Auth Type', { exact: true }).click();
+  await adminPage.getByRole('option', { name: 'FedID' }).click();
+
+  await adminPage
+    .getByLabel('Username *', { exact: true })
+    .fill('frontend_e2e_fedid');
+
+  await adminPage.getByLabel('Authorised Routes', { exact: true }).click();
+
+  await adminPage.getByRole('option', { name: '/users GET' }).click();
+
+  await adminPage.getByRole('button', { name: 'Submit' }).click();
+
+  // filter for frontend_e2e
+
+  await adminPage
+    .getByLabel('Filter by Username', { exact: true })
+    .fill('frontend_e2e');
+
+  // check the routes have been set
+
+  await expect(
+    adminPage.getByText('frontend_e2e_local', { exact: true })
+  ).toHaveCount(1);
+
+  // check the routes have been set
+
+  await expect(adminPage.getByText('/users GET', { exact: true })).toHaveCount(
+    2
+  );
+  // change password for a local user
+
+  await adminPage.getByLabel('Row Actions', { exact: true }).first().click();
+
+  await adminPage.getByText('Change Password', { exact: true }).click();
+
+  await adminPage
+    .getByLabel('Password *', { exact: true })
+    .fill('frontend_e2e_local_password_2');
+
+  await adminPage.getByRole('button', { name: 'Submit' }).click();
+
+  // modify the routes for a fed id user
+
+  await adminPage.getByLabel('Row Actions', { exact: true }).last().click();
+
+  await adminPage
+    .getByText('Modify Authorised Routes', { exact: true })
+    .click();
+
+  await adminPage.getByLabel('Authorised Routes', { exact: true }).click();
+
+  await adminPage.getByRole('option', { name: '/users GET' }).click();
+
+  await adminPage.getByLabel('Authorised Routes', { exact: true }).click();
+
+  await adminPage.getByRole('option', { name: '/maintenance POST' }).click();
+
+  await adminPage.getByRole('button', { name: 'Submit' }).click();
+
+  // check the routes have been set
+
+  await expect(adminPage.getByText('/users GET', { exact: true })).toHaveCount(
+    1
+  );
+
+  await expect(
+    adminPage.getByText('/maintenance POST', { exact: true })
+  ).toHaveCount(1);
+
+  // delete both of the users
+
+  await adminPage.getByLabel('Row Actions', { exact: true }).first().click();
+  await adminPage.getByText('Delete', { exact: true }).click();
+  await adminPage.getByRole('button', { name: 'Continue' }).click();
+
+  await adminPage.getByLabel('Row Actions', { exact: true }).first().click();
+  await adminPage.getByText('Delete', { exact: true }).click();
+  await adminPage.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(
+    adminPage.getByLabel('Row Actions', { exact: true })
+  ).toHaveCount(0);
+});
