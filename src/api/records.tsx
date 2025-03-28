06fb20a84719ba11dd42a68635e2b0329b53a86b
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2025-03-28 12:21:26 +0000

    address review comments

--- src/api/records.tsx
+++ src/api/records.tsx
@@ -96,8 +96,15 @@ const fetchRecords = async (
     }
   });
 
+  const functionDepFunctions = new Set(
+    functionsState.functionsWithDeps
+      .filter((func) => projection?.includes(func.name))
+      .flatMap((func) => func.functions)
+  );
+
   functionsState.functions.forEach((func) => {
-    queryParams.append('functions', JSON.stringify(func));
+    if (Array.from(functionDepFunctions).includes(func.name))
+      queryParams.append('functions', JSON.stringify(func));
   });
 
   const functionChannels = new Set(
