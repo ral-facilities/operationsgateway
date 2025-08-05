6de64b4b2a521912190385b4072ca0e29ecfe056
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2025-08-05 11:42:54 +0000

    Add date range support to plot window date picker in xAxisTab

--- src/plotting/plotSettings/xAxisTab.component.test.tsx
+++ src/plotting/plotSettings/xAxisTab.component.test.tsx
@@ -1,10 +1,15 @@
-import type { RenderResult } from '@testing-library/react';
-import { fireEvent, render, screen, within } from '@testing-library/react';
+import type { QueryClient } from '@tanstack/react-query';
+import { fireEvent, screen, within } from '@testing-library/react';
 import userEvent, { UserEvent } from '@testing-library/user-event';
 import { format } from 'date-fns';
 import { staticChannels } from '../../api/channels';
 import type { FullScalarChannelMetadata } from '../../app.types';
-import { testScalarChannels } from '../../testUtils';
+import type { RootState } from '../../state/store';
+import {
+  getInitialState,
+  renderComponentWithProviders,
+  testScalarChannels,
+} from '../../testUtils';
 import type { XAxisTabProps } from './xAxisTab.component';
 import XAxisTab from './xAxisTab.component';
 
@@ -16,8 +21,15 @@ describe('x-axis tab', () => {
   const changeXMinimum = vi.fn();
   const changeXMaximum = vi.fn();
 
-  const createView = (): RenderResult => {
-    return render(<XAxisTab {...props} />);
+  const createView = (
+    initialState?: Partial<RootState>,
+    queryClient?: QueryClient
+  ) => {
+    const state = getInitialState();
+    return renderComponentWithProviders(<XAxisTab {...props} />, {
+      preloadedState: initialState ?? state,
+      queryClient,
+    });
   };
 
   beforeEach(() => {
