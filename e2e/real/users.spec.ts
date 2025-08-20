836231b230ce98a53af01ee99df8ec582ebadbec
Joshua <83226114+joshdimanteto@users.noreply.github.com>
2025-08-20 10:10:08 +0100

    Apply suggestions from code review
    Remove unnecessary code from the UI mode e2e script and the user e2e test file
    
    Co-authored-by: Louise Davies <louise.davies@stfc.ac.uk>

--- e2e/real/users.spec.ts
+++ e2e/real/users.spec.ts
@@ -1,10 +1,5 @@
 import dotenvx from '@dotenvx/dotenvx';
 import { test as base, BrowserContext, expect, Page } from '@playwright/test';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
 
 dotenvx.config({
   convention: 'nextjs',
