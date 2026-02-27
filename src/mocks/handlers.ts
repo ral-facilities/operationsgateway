e44de1b5f50eb14cc348f76dda7d7f45f3a86ada
Louise Davies <louise.davies@stfc.ac.uk>
2026-02-27 14:52:05 +0000

    DSEGOG-526 Update tests & snapshots

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -274,7 +274,6 @@ export const handlers = [
   http.get('/waveforms/:recordId/:channelName', () => {
     return HttpResponse.json(
       {
-        _id: '1',
         x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         y: [2, 10, 8, 7, 1, 4, 5, 3, 6, 9],
       },
