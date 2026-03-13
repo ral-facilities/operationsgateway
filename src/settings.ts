0abf7dbf4a94cc1cc5c356b6668fe83129741b89
Louise Davies <louise.davies@stfc.ac.uk>
2026-03-13 12:26:59 +0000

    Move `fetchSettings` to settings.ts and move relevant tests - trying to test fetchSettings is a nightmare as we have to deal with all the other main.tsx stuff that happens when you import - so easier to just extract fetchSettings away

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,9 @@
-import type { PluginRoute } from './state/scigateway.actions';
+import axios from 'axios';
+import log from 'loglevel';
+import { MicroFrontendId } from './app.types';
+import { registerRoute, type PluginRoute } from './state/scigateway.actions';
+import LogoDark from '/operationsgateway-logo-white.svg';
+import LogoLight from '/operationsgateway-logo.svg';
 
 export interface WorkingHours {
   start: number;
@@ -20,4 +25,77 @@ export const setSettings = (
   newSettings: Promise<OperationsGatewaySettings | void>
 ): void => {
   settings = newSettings;
+};
+
+export const fetchSettings = (): Promise<OperationsGatewaySettings | void> => {
+  const settingsPath = import.meta.env
+    .VITE_APP_OPERATIONS_GATEWAY_BUILD_DIRECTORY
+    ? import.meta.env.VITE_APP_OPERATIONS_GATEWAY_BUILD_DIRECTORY +
+      'operationsgateway-settings.json'
+    : '/operationsgateway-settings.json';
+  return axios
+    .get<OperationsGatewaySettings>(settingsPath)
+    .then((res) => {
+      const settings = res.data;
+
+      // invalid settings.json
+      if (typeof settings !== 'object') {
+        throw Error('Invalid format');
+      }
+
+      if (!('apiUrl' in settings)) {
+        throw new Error('apiUrl is undefined in settings');
+      }
+
+      // Ensure a limit on how many records can be requested before displaying a warning is present
+      if (!('recordLimitWarning' in settings)) {
+        throw new Error('recordLimitWarning is undefined in settings');
+      }
+
+      if (Array.isArray(settings['routes']) && settings['routes'].length) {
+        settings['routes'].forEach((route: PluginRoute, index: number) => {
+          if ('section' in route && 'link' in route && 'displayName' in route) {
+            const registerRouteAction = {
+              type: registerRoute.type,
+              payload: {
+                section: route['section'],
+                link: route['link'],
+                plugin: 'operationsgateway',
+                displayName: route['displayName'],
+                order: route['order'] ?? 0,
+                hideFromMenu: route['hideFromMenu'] ?? false,
+                admin: route['admin'] ?? false,
+                unauthorised: route['unauthorised'] ?? false,
+                helpSteps:
+                  index === 0 && 'helpSteps' in settings
+                    ? settings['helpSteps']
+                    : [],
+                logoLightMode: settings['pluginHost']
+                  ? settings['pluginHost'] + LogoLight
+                  : undefined,
+                logoDarkMode: settings['pluginHost']
+                  ? settings['pluginHost'] + LogoDark
+                  : undefined,
+                logoAltText: 'OperationsGateway',
+              },
+            };
+            document.dispatchEvent(
+              new CustomEvent(MicroFrontendId, {
+                detail: registerRouteAction,
+              })
+            );
+          } else {
+            throw new Error(
+              'Route provided does not have all the required entries (section, link, displayName)'
+            );
+          }
+        });
+      } else {
+        throw new Error('No routes provided in the settings');
+      }
+      return settings;
+    })
+    .catch((error) => {
+      log.error(`Error loading ${settingsPath}: ${error.message}`);
+    });
 };
